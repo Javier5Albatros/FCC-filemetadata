5cea4de1bd790cc5d6d037c0692ca16ae55a5d65
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-22 00:29:13 +0200

    Done

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
 
-var app = express();
+const port = process.env.PORT || 8080;
+const app = express();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { file } = req;
+  res.json({ name: file.originalname, type: file.mimetype, size: file.size });
+});
 
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
